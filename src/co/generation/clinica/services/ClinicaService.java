7bf197f4c2f69ccc9856568e23346a270f183e6d
Luis Rodriguez <luedropa@gmail.com>
2026-05-08 10:55:08 -0500

    cambios temporales

--- src/co/generation/clinica/services/ClinicaService.java
+++ src/co/generation/clinica/services/ClinicaService.java
@@ -1,6 +1,9 @@
 package co.generation.clinica.services;
 
 import co.generation.clinica.interfaces.Consultable;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ClinicaService implements Consultable {
+    private List<> lista1 = new ArrayList<>();
 }
